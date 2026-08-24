193c8e177843727bbff2295df56a1679994130fb
Jotilla <jfonttroi@gmail.com>
2026-08-24 17:44:27 +0200

    chore: inicializar backend y base de datos

---
+++ backend/src/main/java/com/jotilla/paginatcg/PaginatcgApplication.java
@@ -0,0 +1,13 @@
+package com.jotilla.paginatcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginatcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginatcgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jotilla/paginatcg/PaginatcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotilla.paginatcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginatcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
